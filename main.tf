48f63a32acd6b0efda43871978030c19b08f873c
Sriram27884 <93506259+Sriram27884@users.noreply.github.com>
2023-09-14 17:44:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
+provider "aws" {
+  region = "ap-southeast-1" # Change this to your desired AWS region
+}
 
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "Sriram_Test_ECS" # Change this to your desired cluster name
+}
