d487d5f8c8ccbcf066143aa6806ca6ddb26a414a
Sriram27884 <93506259+Sriram27884@users.noreply.github.com>
2023-09-15 10:22:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,24 @@ provider "aws" {
 resource "aws_ecs_cluster" "my_cluster" {
   name = "Sriram_Test_ECS" # Change this to your desired cluster name
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "DXC/aws"
+      version = "2.70.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key    = "state/terraform_state.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
